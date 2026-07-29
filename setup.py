c3fe843d8923c775a5de14e1716293fd79bd1e32
SomeshD24 <somesh.dalmia013@nmims.in>
2026-07-29 14:51:24 +0530

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kkunal",
-    version="1.1.0",
+    version="1.1.1",
     author="Kkunal",
     description="Kkunal - Python library for Choice FINX Trading API",
     long_description=long_description,
